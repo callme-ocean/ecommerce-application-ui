d0aebbd85edb44f98a8637329122f2f59f09c87f
Sagar <sagarsingh0802@gmail.com>
2023-07-25 19:16:27 +0530

    add bootstrap carousel

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-
+  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
 }

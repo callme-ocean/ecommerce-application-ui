0d0a30a033a09a5f922b4403f80ed98ecc93b826
Sagar <sagarsingh0802@gmail.com>
2023-07-27 23:39:37 +0530

    made search page dynamic

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../services/product.service';
+import { Product } from '../data-type';
 
 @Component({
   selector: 'app-search',
@@ -6,5 +9,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent {
+  searchResult: undefined | Product[];
 
+  constructor(private activeRoute: ActivatedRoute, private product: ProductService) {
+
+  }
+
+  ngOnInit() {
+    let query = this.activeRoute.snapshot.paramMap.get('query');
+    console.warn(query);
+    query && this.product.searchProducts(query).subscribe((result) => {
+      this.searchResult = result;
+    });
+  }
 }
